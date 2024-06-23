7bddf92377ad0c6cda6ebfd7da97bddf68edeefb
kwarchal <kamilwarchal295@gmail.com>
2024-06-23 17:15:53 +0200

    pozostala czesc programu / poprawiona

--- src/trojkiPitagorejskie/Main.java
+++ src/trojkiPitagorejskie/Main.java
@@ -6,9 +6,19 @@ import java.util.stream.Stream;
 class trojkiPitagorejskie{
     public Stream <int[]> Trojki = IntStream.rangeClosed(1,100)
             .boxed()
-            .flatMap(a -> IntStream.rangeClosed(a,100))
-            .filter(b -> Math.sqrt((a*a + b*b)% 1 == 0)
-            .mapToObj(b -> new int[] {a,b, (int)Math.sqrt(a*a+b*b)}));
+            .flatMap(a -> IntStream.rangeClosed(a,100)
+            .filter(b -> Math.sqrt(a*a + b*b) % 1 == 0)
+            .mapToObj(b -> new int[] {a, b, (int)Math.sqrt(a*a+b*b)}));
 }
 public class Main {
+    public static void main(String[] args)
+    {
+        trojkiPitagorejskie pt = new trojkiPitagorejskie();
+
+        System.out.println("Program generuje 10 trojek pitagorejskich: \n");
+
+        pt.Trojki
+                .limit(10)
+                .forEach(t -> System.out.println("(" + t[0] + "," + t[1] + ", " + t[2] +")"));
+    }
 }
